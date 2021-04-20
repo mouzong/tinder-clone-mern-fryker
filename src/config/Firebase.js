4a237c18a1f44ac9875e70b97fe7e3d6b43095cf
mouzong <andreasmouzong@gmail.com>
2021-04-20 16:34:43 +0100

    added firebase to the project

---
+++ src/config/Firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCwK3qfq4tTDObGu7e4Sjk8kJIjqzAwvSI",
+  authDomain: "tinder-clone-fryker-4d8be.firebaseapp.com",
+  projectId: "tinder-clone-fryker-4d8be",
+  storageBucket: "tinder-clone-fryker-4d8be.appspot.com",
+  messagingSenderId: "630961726748",
+  appId: "1:630961726748:web:520e128b7fc4d7bfe5c6d9",
+  measurementId: "G-VZ0013JKML",
+};
+
+export default firebaseConfig;
