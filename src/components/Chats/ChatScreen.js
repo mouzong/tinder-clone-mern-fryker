e3677344c9ee064a94ea83a1c21bb709cc36ad6e
mouzong <andreasmouzong@gmail.com>
2021-04-22 16:35:28 +0100

    added the chat screen and the input message function

--- src/components/Chats/ChatScreen.js
+++ src/components/Chats/ChatScreen.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import "./ChatScreen.css";
 
 const ChatScreen = () => {
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([
     {
       name: "Elon Musk",
@@ -19,9 +22,43 @@ const ChatScreen = () => {
     },
   ]);
 
+  const handleSend = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { message: input }]);
+  };
+
   return (
     <div className="chatScreen">
-      <h2>Chat Screen</h2>
+      <p className="chatScreen__timestamp">YOU MATCHED WITH ELON ON 21/04/21</p>
+      {messages.map((message) =>
+        message.name ? (
+          <div className="chatScreen__message">
+            <Avatar
+              className="chatScreen__image"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
+        ) : (
+          <div className="chatScreen__message">
+            <p className="chatScreen__textUser">{message.message}</p>
+          </div>
+        )
+      )}
+
+      <form className="chatScreen__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="chatScreen__inputField"
+          placeholder="Type a message"
+          type="text"
+        />
+        <button onClick={handleSend} className="chatScreen__inputButton">
+          SEND
+        </button>
+      </form>
     </div>
   );
 };
